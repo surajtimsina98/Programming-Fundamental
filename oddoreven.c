7df58efbedf08ae4d55db79d48c0ae0dc15b4141
surajtimsina98 <suraj.timsina98@gmail.com>
2016-12-09 10:59:09 +0545

    Add files via upload

---
+++ oddoreven.c
@@ -0,0 +1,15 @@
+/* student name: suraj timsina
+   roll no.:
+   program: WAP to print largest number among three number using ternary operator
+   lab sheet: 11
+   date: dec.9 2016
+*/
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter the number:\n");
+    scanf("%d",&a);
+    (a%2==0)?printf("number is even"):printf("number is odd");
+    return 0;
+}
