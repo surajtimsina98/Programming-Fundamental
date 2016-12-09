768f863a9dc9d0e46d8bfcb5849ea34c7bc62a21
surajtimsina98 <suraj.timsina98@gmail.com>
2016-12-09 11:13:07 +0545

    Add files via upload

---
+++ laegestamongtwo.c
@@ -0,0 +1,17 @@
+/* student name: suraj timsina
+   roll no.:46
+   program: WAP to print largest number among three number using ternary operator
+   lab sheet: 11
+   date: dec.9 2016
+*/
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the first number");
+    scanf("%d",&a);
+    printf("enter the second number");
+    scanf("%d",&b);
+    (a>=b)?printf("a is greater"):printf("b is greater");
+    return 0;
+}
