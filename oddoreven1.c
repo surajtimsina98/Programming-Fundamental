9d7e13567c7cd824ab84589ba9da6d3fed22349e
surajtimsina98 <suraj.timsina98@gmail.com>
2016-12-12 11:36:38 +0545

    Add files via upload

---
+++ oddoreven1.c
@@ -0,0 +1,29 @@
+/*
+Name:Suraj Timsina
+Subject:Programming Fundamental
+Rollno.-46
+Lab Sheet No.-11
+Program-WAP
+Date-12 Dec. 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter the number \n");
+scanf("%d",&a);
+
+if(a%2==0)
+{
+
+
+printf("number is even");
+}
+else
+{
+    printf("number is odd");
+}
+getch();
+return 0;
+}
