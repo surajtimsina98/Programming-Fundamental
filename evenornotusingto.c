b2f37334d5cc348054cdf09407e78192e7d68a15
surajtimsina98 <suraj.timsina98@gmail.com>
2017-01-09 11:08:35 +0545

    Add files via upload

---
+++ evenornotusingto.c
@@ -0,0 +1,19 @@
+/*
+Name:Suraj Timsina
+Subject:Programming Fundamentals
+Program:WAP number is odd or even using ternary operator
+Lab Sheet:16
+Date:8 Jan, 2017
+*/
+
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter a number");
+scanf("%d",&a);
+
+(a%2==0)?printf("The number is even \n"):printf("The number is odd \n");
+
+return 0;
+}
