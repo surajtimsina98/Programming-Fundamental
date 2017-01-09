1538dff819f7227d277044b4e19dd4bc12aa52d9
surajtimsina98 <suraj.timsina98@gmail.com>
2017-01-09 12:58:57 +0545

    Add files via upload

---
+++ Project_Proposal.c
@@ -0,0 +1,16 @@
+PROPOSAL
+
+-Abstract
+-Introduction
+-Objective
+-Literature Review
+-Problem Statement
+-Methodology
+-Algorithm/Flowchart
+-Use Case Diagram
+-Time Estimation
+-Gantt Chart
+-Cost Estimation
+-Expected Output
+-Scope and Limitation
+-Reference
