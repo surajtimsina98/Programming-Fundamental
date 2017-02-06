2c686a9824f401153cc7650e55938aa15cf09a4e
surajtimsina98 <suraj.timsina98@gmail.com>
2017-02-06 11:13:46 +0545

    Add files via upload

---
+++ pointer1.c
@@ -0,0 +1,16 @@
+/*
+student name: Suraj Timsina
+program: pointer 1
+lab sheet: 25
+date: Feb.3 2017
+*/
+#include<stdio.h>
+int main()
+{
+int x=5;
+int*p;
+p=&x;
+printf("the memory address of x variable is %d",p);
+
+return 0;
+}
